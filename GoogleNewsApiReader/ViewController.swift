f5328fa5b6e1541863e9cd721a50512dcce47f77
Blake Barrett <blake@blakebarrett.net>
2015-10-27 23:16:56 -0700

    Added cocoapods and AlamoFire

--- GoogleNewsApiReader/ViewController.swift
+++ GoogleNewsApiReader/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request(.GET, "https://ajax.googleapis.com/ajax/services/search/news", parameters: ["q": "barack%20obama", "v": "1.0"])
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
     }
 
     override func didReceiveMemoryWarning() {
